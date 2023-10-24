7583d17fb835b81abbd3c294d0ee48c63d553bc2
Qhuy03 <huyntq2003@gmail.com>
2023-10-24 20:35:02 +0700

    Huy

---
+++ src/pages/Home/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {Link, useNavigate} from "react-router-dom";
+
+const Header =() => {
+    const navigate = new useNavigate();
+
+    return(
+        <>
+        <div className="navBar">
+            
+        </div>
+        </>
+    );
+};
